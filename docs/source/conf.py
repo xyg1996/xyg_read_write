9087007ffba64d7737f066cc59fe6689bc6a50d2
110% <xtmals@163.com>
2022-04-29 16:58:44 +0800

    uml figs and plan to use mermaid!

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,13 +16,14 @@
 
 
 # -- Project information -----------------------------------------------------
+import sphinx_theme
 
-project = 'simforge_docs'
-copyright = '2022, simforge'
-author = 'simforge应用组'
+project = 'xyg的阅读与记录'
+copyright = '2022, wuxi'
+author = 'xyg'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,9 +31,6 @@ release = '2.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'myst_parser',
-]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -60,27 +58,40 @@ extensions = [
     "nbsphinx", # MyST-NB
     "sphinx.ext.autodoc",# 引用程序语言函数  
     "sphinx.ext.intersphinx",
+    "sphinxcontrib.plantuml",
+    "sphinxcontrib.mermaid"
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None)
 }
 
+plantuml = 'java -jar ./utils/plantum.jar'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build']
 
+source_suffix = {
+    '.md': 'markdown'
+}
+
+mermaid_output_format = 'raw'
+mermaid_version = ''
+html_js_files = [
+    'js/mermaid.js'
+]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'stanford_theme'
+html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
