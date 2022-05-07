499723a6ee14abcad78fc3d7d0cbaf5c1ed24745
110% <xtmals@163.com>
2022-05-07 12:24:03 +0800

    pdf output test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,9 +90,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'stanford_theme'
-#html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
+#html_theme = 'stanford_theme'
+html_theme = 'sphinx_rtd_theme'
+#html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
